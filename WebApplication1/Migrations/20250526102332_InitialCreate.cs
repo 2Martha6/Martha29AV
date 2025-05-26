10b6ca858524fb32061104465a192ee40e574a2d
2Martha6 <madrigalmartha111@gmail.com>
2025-05-26 10:36:32 -0500

    correcion de BD

---
+++ WebApplication1/Migrations/20250526102332_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Majo29AV.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "PkRol",
+                keyValue: 1,
+                column: "Nombre",
+                value: "Director");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "PkRol",
+                keyValue: 1,
+                column: "Nombre",
+                value: "sa");
+        }
+    }
+}
